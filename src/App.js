a15031d701a23b773101dcbb7ad67226f3b7db1e
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-11-17 20:25:03 +0530

    BubbleSort Implemented

--- src/App.js
+++ src/App.js
@@ -11,81 +11,69 @@ function App() {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    function componentDidMount() {
-        resetArray();
-    }
+
 
     function resetArray() {
         const array = [];
         let Number_OF_Array = document.getElementById("ARRAY_SIZE").value;
         for (let i = 0; i < Number_OF_Array; i++) {
-            array.push(randomIntFromInterval(5, 500));
+            array.push(randomIntFromInterval(5, 400));
             setArray(array)
         }
     }
-function bubbleSort() {
-    let ANIMATION_SPEED = document.getElementById("DELAY").value; //animation technique starting
-    const animations = BubbleSortAlgorithm(array);
-    console.log(animations);
-    for (let i = 0; i < animations.length; i++) {
-        const arrayBars = document.getElementsByClassName("array-bar");
-        const isColor = i % 4 !== 2 && i % 4 !== 3;
-        if (isColor) {
-            const [barOneIdx, barTwoIdx] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            const color = i % 4 === 0 ? SECONDARY_COLOUR : PRIMARY_COLOUR;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-            }, i * ANIMATION_SPEED);
-        } else {
-            setTimeout(() => {
-                const [barOneIdx, barTwoIdx] = animations[i]; //barTwoIdx== new height
+
+    function bubbleSort() {
+        let ANIMATION_SPEED = document.getElementById("DELAY").value;
+        const animations = BubbleSortAlgorithm(array);
+        console.log(animations);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName("array-bar");
+            const isColor = i % 4 !== 2 && i % 4 !== 3;
+            if (isColor) {
+                const [barOneIdx, barTwoIdx] = animations[i];
                 const barOneStyle = arrayBars[barOneIdx].style;
-                barOneStyle.height = `${barTwoIdx}px`;
-            }, i * ANIMATION_SPEED);
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 4 === 0 ? SECONDARY_COLOUR : PRIMARY_COLOUR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, barTwoIdx] = animations[i]; //barTwoIdx== new height
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${barTwoIdx}px`;
+                }, i * ANIMATION_SPEED);
+            }
         }
     }
-}
+
     return <div className={"app"}>
         <div className="array-container">
-            {array.map(
-                (
-                    value,
-                    ids //mapping bars
-                ) => (
-                    <div
+            {array.map((value, index) => {
+                    return <div
                         className="array-bar"
-                        key={ids}
+                        key={index}
                         style={{
                             backgroundColor: PRIMARY_COLOUR,
                             height: `${value}px`,
                         }}
                     />
-                )
+                }
             )}
         </div>
         <div className="footer">
-            {/* <input
-            className="inp"
-            id="inpt"
-            type="text"
-            placeholder="Enter comma seperated value (5-500)"
-            maxLength={200}
-          ></input> */}
             <label htmlFor="array-size">Size</label>
             <input
                 id="ARRAY_SIZE"
                 name="array-size"
                 type="range"
                 min="5"
-                max="100"
+                max="80"
             />
             <label htmlFor="delay">Delay</label>
             <input id="DELAY" name="delay" type="range" min="3" max="100"/>
             <button onClick={() => resetArray()}>Generate Array</button>
-
             <button onClick={() => bubbleSort()}>Bubble Sort</button>
 
         </div>
--- src/BubbleSort/BubbleSortAlgo.js
+++ src/BubbleSort/BubbleSortAlgo.js
@@ -10,14 +10,14 @@ function bubbleSort(array, n, animations) {
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                animations.push([j, j + 1]);                            //changing colour to red
+                animations.push([j, j + 1]);           //changing colour to red
                 animations.push([j, j + 1]);
 
                 let temp = array[j];
                 animations.push([j, array[j + 1]]);
-                animations.push([j + 1, temp]);                          //for swapping values of bar
+                animations.push([j + 1, temp]);          //for swapping values of bar
                 array[j] = array[j + 1];
-                array[j + 1] = temp;                                     //for swapping values in original array
+                array[j + 1] = temp;                    //for swapping values in original array
 
             }
         }
